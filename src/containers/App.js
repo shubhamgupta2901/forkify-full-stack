61ce967664479fae741fcac03c41510d72330261
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-07-29 14:22:23 +0530

    Reorganized container in a different folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './App.module.css';
-import Header from './components/Header/Header';
-import Results from './components/Results/Results'
-import Recipe from './components/Recipe/Recipe';
-import Shopping from './components/Shopping/Shopping';
+import Header from '../components/Header/Header';
+import Results from '../components/Results/Results'
+import Recipe from '../components/Recipe/Recipe';
+import Shopping from '../components/Shopping/Shopping';
 
 function App() {
   return (
