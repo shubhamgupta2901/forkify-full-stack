120f12fd316e95e15074451e98e481825eb59f50
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-07-29 15:28:16 +0530

    Added Loader

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SVGIcon from './SVGIcon';
+
+const loader = () =>{
+    return(
+        <div className="loader">
+            <SVGIcon name="cw"/> 
+        </div>
+    )
+}
+export default loader;
