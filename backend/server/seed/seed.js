890375d6baa73ec8ea502d4e194bbfe2bc8e2d3e
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-11-25 17:20:50 +0530

    Seeding db complete

--- backend/server/seed/seed.js
+++ backend/server/seed/seed.js
@@ -1,30 +1,55 @@
 const fs = require('fs');
 const path = require('path');
-// require('../db/mongoose');
-// const Recipe = require('../models/recipe');
+const util = require('util');
+require('../db/mongoose');
+const Recipe = require('../db/models/recipeModel');
+const Publisher = require('../db/models/publisherModel');
 
-const readRecipeFromTemplate = ()=> {
-   
-    // const filePath = path.join(__dirname,'./recipeTemplate.json');
-    console.log(`Attempting to read file: ${filePath}`);
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array);
+    }
+}
+
+const readFile = util.promisify(fs.readFile);
+const readRecipesFromJson= async ()=> {
     try {
-        const dataBuffer = fs.readFileSync(filePath);
+        const filePath = path.join(__dirname,'./clean_db.json');
+        const dataBuffer = await readFile(filePath);
         const dataJSON = dataBuffer.toString();
         const dataObj =  JSON.parse(dataJSON);
         return dataObj;
     } catch (error) {
-        console.log(error+'. Returning empty Array.');
-        return [];
+        console.log(error);
+        return null;
     }
 }
 
-const attemptSeeding = () => {
-    const recipe = readRecipeFromTemplate();
-    new Recipe(recipe).save()
-        .then(response=> console.log(response))
-        .catch(error=> console.log(error.message));
+const attemptSeeding = async () => {
+    try {
+        const recipeJson = await readRecipesFromJson();
+        const recipes = recipeJson.recipes;
+        asyncForEach(recipes,async (recipe)=>{
+            let publisher;
+            publisher = await Publisher.findOne({name: recipe.source});
+            if(!publisher){
+                publisher = await new Publisher({name:recipe.source}).save();
+                console.log(`Publisher created: ${publisher._id}`)
+            }
+            recipe.publisher = publisher._id;
+            const recipeResponse  = await new Recipe(recipe).save();
+            console.log(`Recipe created: ${recipeResponse._id}`)
+        });
+        
+    } catch (error) {
+        console.log(`Error while seeding!`,error);
+    }
+    
+}
+const performSeeding = async ()=> {
+    await attemptSeeding();
+    console.log('Seeding complete');
 }
 
-// attemptSeeding();
+performSeeding();
 
-console.log(assetsDirectory);
